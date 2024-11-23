7e4140e8f9433445f1721b0b835513176087fb2f
Richard Wise <richard.wise@hey.com>
2024-11-22 21:15:33 -0500

    add relationship of employee->facility

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ApplicationRecord
   PREFIXES = %w[ Dr Professor Mr Miss Mrs ]
 
   belongs_to :location
+  has_one :facility, through: :location
   has_many :contacts, as: :contactable
 
   def display_name
